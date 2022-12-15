955b3f64a713380aa6124912ce4122ae5ae5a9fd
CodeMarcos <code.henrique@gmail.com>
2022-12-15 10:08:25 -0300

    Update path values

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mongoClient = require ("mongodb").MongoClient;
-mongoClient.connect("mongodb://localhost:27017",
+mongoClient.connect("mongodb://0.0.0.0:27017",
 	                {useUnifiedTopology: true},
 	                (error,connection)=>{
 	                	if(error) return console.log(error);
-	                	global.connection = connection.db("Client");
+	                	const db = client.db('nodeCrud');
+  						const collection = db.collection('Client');
 	                	console.log("connect!");
 	                });
 
-module.exports = {}
+function findAll() {
+    return global.conn.collection("customers").find().toArray();
+}
+ 
+module.exports = { findAll }
